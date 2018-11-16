277e8ed65cc9e8bcf8a26d2b6de11b1cc30ed09a
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 07:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for(let i = 0; i < cart.length; i++) {
+    if(item === cart[i].itemName) {
+      cart.splice()
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
