0e4718b2b8f9ddb4e64f2b1a923047303ccaa350
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 07:40:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ function viewCart() {
 
 function total() {
   // write your code here
+  let total = 0;
+  for(let i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
