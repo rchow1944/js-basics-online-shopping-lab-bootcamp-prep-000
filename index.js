e69576b2744a844c9b72fac05365d59c55ba316b
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 07:48:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  return 'That item is not in your cart';
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
